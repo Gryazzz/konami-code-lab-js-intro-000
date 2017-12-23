f355b0fa2632b30e453d4910362686f077960881
Sonik S <gryazzz@gmail.com>
2017-12-23 01:49:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  function init() {
+  var i = 0;
+  document.body.addEventListener('keydown', function(e) {
+  var key = parseInt(e.detail || e.which);
+  if (key === code[i]) {
+    i++;
+    if (i === code.length) {
+      alert('You did it!');
+      i = 0;
+    } else {
+      i = 0;
+    }
+  }
+});
+}
 
 }
