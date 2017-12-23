1bc7097e5a631ae4a5242fbe28e4d3a3a8ecc29f
Sonik S <gryazzz@gmail.com>
2017-12-23 02:13:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let i = 0;
-  const key = parseInt(e.detail || e.which);
-
+  
   document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
     if (key === code[i]) {
       i++;
 
