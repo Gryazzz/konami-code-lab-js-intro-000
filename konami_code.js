4c073ac30702fc07d0069446262e6f602fd67235
Sonik S <gryazzz@gmail.com>
2017-12-23 02:12:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  var i = 0;
-  var body = document.querySelector('body');
-  body.addEventListener('keydown', function(e) {
-  var key = parseInt(e.detail || e.which);
-  if (key === code[i]) {
-    i++;
+  let i = 0;
+  const key = parseInt(e.detail || e.which);
+
+  document.body.addEventListener('keydown', function(e) {
+    if (key === code[i]) {
+      i++;
+      
     if (i === code.length) {
       alert('You did it!');
       i = 0;
     } else {
+      alert('wrong');
       i = 0;
     }
   }
